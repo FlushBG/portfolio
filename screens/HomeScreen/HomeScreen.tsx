e12b7827853b84c9c15b5f9c15c8eefe3eca5cae
Angel Angelov <flushbg@gmail.com>
2023-02-20 13:39:25 +0200

    Add greeting to homescreen

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { useScroll } from 'framer-motion';
+import { motion, useScroll } from 'framer-motion';
 import { imageData } from './image-data';
 import ParallaxImage from '../../components/ParallaxImage/ParallaxImage';
 import styles from './HomeScreen.module.scss';
+import useParallax from '../../hooks/useParallax';
 
 const HomeScreen: React.FC = () => {
   const screenRef = React.useRef<HTMLDivElement>(null);
@@ -10,25 +11,31 @@ const HomeScreen: React.FC = () => {
     target: screenRef,
     offset: ['end end', 'end start'],
   });
+  const y = useParallax(scrollYProgress, 0, 350);
 
   return (
-      <div ref={screenRef} className={styles.body}>
-        <div className={styles.bottomBar} />
-        {/* <div className={styles.title}>Hi, I&apos;m</div>
+    <div ref={screenRef} className={styles.body}>
+      <div className={styles.bottomBar} />
+      <motion.div style={{ y }} className={styles.text}>
+        <fieldset className={styles.intro}>
+          <legend>Hi, I&apos;m</legend>
+        </fieldset>
+        {/* <div className={styles.intro}>Hi, I&apos;m</div> */}
         <div className={styles.title}>Angel Angelov</div>
-        <div className={styles.title}>Full-Stack Developer</div> */}
-        {imageData.map((data) => (
-          <ParallaxImage
-            key={data.alt}
-            className={styles[data.className]}
-            src={data.src}
-            alt={data.alt}
-            scrollProgress={scrollYProgress}
-            start={data.start}
-            distance={data.distance}
-          />
-        ))}
-      </div>
+        <div className={styles.subtitle}>Full-Stack Dev</div>
+      </motion.div>
+      {imageData.map((data) => (
+        <ParallaxImage
+          key={data.alt}
+          className={styles[data.className]}
+          src={data.src}
+          alt={data.alt}
+          scrollProgress={scrollYProgress}
+          start={data.start}
+          distance={data.distance}
+        />
+      ))}
+    </div>
   );
 };
 
